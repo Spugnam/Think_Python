9c4f40bcacdbc6473e228b4c9f2ec9ead603c833
Spugnam <quentin.picard@gmail.com>
2017-06-27 11:11:05 -0400

    Created sed function (file copy/ replacement)

--- wc.py
+++ wc.py
@@ -1,14 +1,8 @@
-import sys, os
-sys.path.append('/Users/Quentin/Documents/Python/Think Python')
-
 def linecount(filename):
     count = 0
     for line in open(filename):
         count += 1
     return count
-
-print(linecount('/Users/Quentin/Documents/Python/Think Python/wc.py'))
-
 
 if __name__ == '__main__':
     print(linecount('wc.py'))
